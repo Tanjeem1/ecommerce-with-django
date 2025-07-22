fbf72691020109b19cc2743ba6a13693206eca3a
tanjeem <tanjeemhridoy93@gmail.com>
2025-07-22 22:21:26 +0530

    correct the read me file

--- furniture/settings.py
+++ furniture/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY ='django-insecure-bp9+-6vh1u$-9qn5-*hdja@3^5x^fn#py1an_6wcax+1t3m%0@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
@@ -141,8 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
